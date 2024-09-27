3197d842e8f6f6aa34de900ac0731abd979a86ae
wjxor <shko940575@gmail.com>
2024-09-27 11:28:32 +0000

    푸쉬

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,14 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+    apiKey: "AIzaSyDHh1B500FYM7a9YqXzIm4zU4tfiT3mm_A",
+    authDomain: "mobile-invitaion-c3174.firebaseapp.com",
+    databaseURL: "https://mobile-invitaion-c3174-default-rtdb.firebaseio.com",
+    projectId: "mobile-invitaion-c3174",
+    storageBucket: "mobile-invitaion-c3174.appspot.com",
+    messagingSenderId: "847636899124",
+    appId: "1:847636899124:web:e4f3909bd6271115b1b161",
+    measurementId: "G-JVH93T1XH9"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
